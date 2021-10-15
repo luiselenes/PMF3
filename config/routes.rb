c8f5ae511b6218612a30e8de921c6e743586794b
luiselenes <luiseelenes@outlook.com>
2021-10-15 16:14:17 -0600

    cambio de ruta inicio

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   devise_scope :user do
-    root to: "devise/sessions#new"
+    root to: "routes#index"
   end
   resources :imagens
   resources :routes
