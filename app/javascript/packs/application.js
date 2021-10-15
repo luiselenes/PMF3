c7b4208148863993328969330f3d6bdf204dc501
luiselenes <luiseelenes@outlook.com>
2021-10-15 15:28:40 -0600

    application resuelto

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,27 +2,5 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-<<<<<<< HEAD
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "../../assets/stylesheets/application";
-
-
-document.addEventListener("turbolinks:load",function(){
-    $(function(){
-        $('[data-toggle="tooltip"]').tooltip()
-        $('[data-toggle="popover"]').popover()
-    })
-})
-
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-=======
 // = requiere jquery 
 // = requiere bootstrap-sprockets
->>>>>>> feature/luis/login
