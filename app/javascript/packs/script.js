539a17ba0b2b5a7ca5042a0240fd33a5e9ac9634
luiselenes <luiseelenes@outlook.com>
2021-10-22 18:37:47 -0600

    funciona script

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,13 +1,11 @@
- // This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-import $ from 'jquery';
-import bootstrap from 'bootstrap';
-import fontawesome from 'font-awesome'
+// 
+// Scripts
+// 
 
-console.log('hola')
-$(document).on("turbolinks:load", function(){
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
@@ -18,8 +16,7 @@ $(document).on("turbolinks:load", function(){
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-            console.log('Hola mundo')
         });
     }
-})
-console.log('h0lawa')
+
+});
