e632bfea1f77cc0e0335c48118d2a538b4d496b9
CarlosaoVillegas <carlos.a.o.villegas@gmail.com>
2021-10-29 11:15:10 -0600

    agregar db faltantes

---
+++ db/migrate/20211026200437_create_paths.rb
@@ -0,0 +1,12 @@
+class CreatePaths < ActiveRecord::Migration[6.1]
+  def change
+    create_table :paths, :primary_key => false ,:id=>false do |t|
+      t.float :lat
+      t.float :lng
+      t.boolean :sensor
+      t.references :route, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
